78bf700f809c963066a9dfe131bc88bffa3f246f
Muhsin <muhsintt777@gmail.com>
2025-08-19 21:15:58 +0530

    fix: lint warning

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,19 +1,21 @@
-import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import express, { json } from 'express';
 import morgan from 'morgan';
+
 import { corsOptions } from 'configs/cors';
-import { appRouter } from './router';
-import { notFoundHandler } from './not-found-handler';
-import { healthHandler } from './health-handler';
+
 import { errorHandler } from './error-handler';
+import { healthHandler } from './health-handler';
 import { metaDataHandler } from './meta-data';
+import { notFoundHandler } from './not-found-handler';
+import { appRouter } from './router';
 
 const app = express();
 
 app.use(morgan(':method :url :status'));
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(json());
 app.use(cookieParser());
 app.use('/api', appRouter);
 app.use('/metadata', metaDataHandler);
